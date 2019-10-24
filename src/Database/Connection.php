210a52c4de2d86be25f76235d2c46082315cd16c
Roman Hrab <lroman242@gmail.com>
2019-10-24 12:19:48 +0300

    use correct grammar

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -9,6 +9,23 @@ use Libern\LaravelClickHouse\Database\Query\Builder;
 
 class Connection extends \Tinderbox\ClickhouseBuilder\Integrations\Laravel\Connection
 {
+    public function __construct(array $config)
+    {
+        parent::__construct($config);
+
+        $this->queryGrammar = $this->getDefaultQueryGrammar();
+    }
+
+    /**
+     * Get the default query grammar instance.
+     *
+     * @return \Tinderbox\ClickhouseBuilder\Query\Grammar
+     */
+    protected function getDefaultQueryGrammar()
+    {
+        return new Grammar();
+    }
+    
     public function query()
     {
         return new Builder($this, new Grammar());
